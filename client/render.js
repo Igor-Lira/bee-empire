9cabe9b6ec6265bb605e76e3148da5d50e053e35
ilirapassos <igor.lira-passos@njuko.com>
2023-12-08 23:25:58 +0100

    [#1]: render honeycomb

---
+++ client/render.js
@@ -0,0 +1,42 @@
+const cv = document.getElementById('canvas');
+const ctx = cv.getContext('2d');
+cv.width = window.innerWidth;
+cv.height = window.innerHeight;
+
+function drawHexagon(x, y, size) {
+  ctx.beginPath();
+  ctx.moveTo(x, y + size*Math.sqrt(3));
+  ctx.lineTo(x + 1.5*size, y + Math.sqrt(3)*size/2);
+  ctx.lineTo(x + 1.5*size, y - Math.sqrt(3)*size/2);
+  ctx.lineTo(x, y - size*Math.sqrt(3));
+  ctx.lineTo(x - 1.5*size, y - Math.sqrt(3)*size/2);
+  ctx.lineTo(x - 1.5*size, y + Math.sqrt(3)*size/2);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function drawHoneycomb(rows, cols, hexSize, xOffset, yOffset) {
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let x;
+      if (row % 2 === 0) {
+        x = xOffset + col * 2*hexWidth;
+      } else {
+        x = xOffset + hexWidth + col *2*hexWidth;
+      }
+      const y = yOffset + row * 1.5*hexHeight;
+      drawHexagon(x, y, hexSize);
+    }
+  }
+}
+
+const hexSize = 30;
+const hexHeight = hexSize * Math.sqrt(3);
+const hexWidth = hexSize * 1.5;
+const rows = 10;
+const cols = 10;
+const xOffset = 50;
+const yOffset = 50;
+
+// Draw the honeycomb pattern
+drawHoneycomb(rows, cols, hexSize, xOffset, yOffset);
